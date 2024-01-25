dbd7fc774fa414e8336f2ecc134e92a17954c709
Sacjo <dani.sanchez.13.ds@gmail.com>
2024-01-25 20:28:17 -0300

    agg section autos

---
+++ src/Components/Home/Autos.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Cards from '../cards.jsx';
+
+const Autos = () => {
+    const autos = [
+        { link:"https://www.google.com", img: "https://cdn.autobild.es/sites/navi.axelspringer.es/public/bdc/dc/fotos/KIA_Niro_01_0.jpg?tf=1080x", nombre: "Auto 1", precio: "20.000", km: "10.000" },
+        { link:"https://www.google.com", img: "https://cdn.autobild.es/sites/navi.axelspringer.es/public/bdc/dc/fotos/KIA_Niro_01_0.jpg?tf=1080x", nombre: "Auto 2", precio: "30.000", km: "5.000" },
+        { link:"https://www.google.com", img: "https://cdn.autobild.es/sites/navi.axelspringer.es/public/bdc/dc/fotos/KIA_Niro_01_0.jpg?tf=1080x", nombre: "Auto 2", precio: "30.000", km: "5.000" },
+        { link:"https://www.google.com", img: "https://cdn.autobild.es/sites/navi.axelspringer.es/public/bdc/dc/fotos/KIA_Niro_01_0.jpg?tf=1080x", nombre: "Auto 2", precio: "30.000", km: "5.000" },
+        { link:"https://www.google.com", img: "https://cdn.autobild.es/sites/navi.axelspringer.es/public/bdc/dc/fotos/KIA_Niro_01_0.jpg?tf=1080x", nombre: "Auto 2", precio: "30.000", km: "5.000" },
+        { link:"https://www.google.com", img: "https://cdn.autobild.es/sites/navi.axelspringer.es/public/bdc/dc/fotos/KIA_Niro_01_0.jpg?tf=1080x", nombre: "Auto 2", precio: "30.000", km: "5.000" },
+        { link:"https://www.google.com", img: "https://cdn.autobild.es/sites/navi.axelspringer.es/public/bdc/dc/fotos/KIA_Niro_01_0.jpg?tf=1080x", nombre: "Auto 2", precio: "30.000", km: "5.000" },
+        { link:"https://www.google.com", img: "https://cdn.autobild.es/sites/navi.axelspringer.es/public/bdc/dc/fotos/KIA_Niro_01_0.jpg?tf=1080x", nombre: "Auto 2", precio: "30.000", km: "5.000" },
+    ];
+    return (
+        <section id='autos' className='mb-5'>
+            <div className='container'>
+                <div className='row justify-content-center'>
+                    <div className='col-10 col-md-6 mb-3 mt-2'>
+                        <h2 className='text-center'>Autos</h2>
+                    </div>
+                    <div className='row justify-content-center justify-content-md-between'>
+                        {autos.map((auto, index) => (
+                            <Cards
+                                key={index}
+                                link={auto.link}
+                                img={auto.img}
+                                nombre={auto.nombre}
+                                precio={auto.precio}
+                                km={auto.km}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Autos
